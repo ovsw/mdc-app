85db878387e614468445b6b3c01679df293b293d
Ovi Savescu <ovi@ovswebsites.com>
2019-01-21 20:39:06 +0200

    basic page basic content styles

--- src/components/Templates/BasicPageTemplate.js
+++ src/components/Templates/BasicPageTemplate.js
@@ -7,20 +7,39 @@ const AboutPage = ({ title, bannerImage, body }) => (
   <div className="mt-24">
     {/* <img src={`${data.datoCmsAboutPage.bannerImage.url}?auto=compress&fit=facearea&h=400&w=1200`} /> */}
     {bannerImage != 'missingImage' && <Img fluid={bannerImage.fluid} />}
-    <div className="px-6 md:px-8 xl:px-16 mt-4 mx-auto" style={{ background: 'url()' }}>
-      <h1>This is a basic page with title: {title}</h1>
+
+    <div className="mainContentWrapper" style={{ background: 'url()' }}>
+      <div className="container mx-auto">
+        <h1 className="narrowContent">{title}</h1>
+      </div>
+
       {body.map(block => (
         <div key={block.id}>
           {block.model.apiKey === 'rich_text' && (
             <div className="container mx-auto">
               <div
-                className="px-2 md:px-16 lg:px-32 mx-12"
+                className="narrowContent"
                 dangerouslySetInnerHTML={{
                   __html: block.text,
                 }}
               />
             </div>
           )}
+
+          {block.model.apiKey === 'quote' && (
+            <div className="container mx-auto narrowContent">
+              <blockquote>
+                <span className="bqstart">“</span>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: block.text,
+                  }}
+                />
+                <span className="bqend">”</span>
+              </blockquote>
+            </div>
+          )}
+
           {block.model.apiKey === 'image' && <Img fluid={block.image.fluid} className="container mx-auto mb-8" />}
         </div>
       ))}
