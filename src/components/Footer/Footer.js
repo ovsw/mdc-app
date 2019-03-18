671d3caeed0792cf78e00b38f5a020faf894a3da
Ovi Savescu <ovi@ovswebsites.com>
2019-03-18 12:34:09 +0200

    updated footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,46 +1,100 @@
 import React from 'react'
+import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookSquare, faTwitterSquare, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
-import styles from './Footer.module.css'
+const FooterStyled = styled.footer`
+  ${tw`px-2 py-4 mt-0 bg-green-brand text-white`};
+  ${tw`xl:w-1/2 xl:mr-auto xl:px-8`};
+  box-shadow: inset -5px 0 10px rgba(51, 50, 15, 0.23);
+
+  a {
+    color: ${props => props.theme.secondaryColorLight};
+
+    &:hover {
+      ${tw`text-yellow-brand`};
+    }
+  }
+`
+const Wrapper = styled.div`
+  ${tw`container mx-auto md:flex`};
+`
+const LeftColumn = styled.div`
+  ${tw`text-sm md:text-left pt-6`};
+`
+const RightColumn = styled.div`
+  ${tw`flex-1 md:pt-4`};
+`
+const Icons = styled.div`
+  ${tw`flex justify-center md:justify-end px-10 md:px-0`};
+`
+
+const SocialIcon = styled.a`
+  color: white !important;
+  ${tw`text-3xl mx-4`};
+  ${tw`md:ml-2 md:mr-0`};
+  &:hover {
+    color: ${props => props.theme.secondaryColorLight}!important;
+  }
+`
+const SeoText = styled.p`
+  ${tw`container mt-4 text-xs`};
+  color: #a2c5a6;
+`
+const CopyRight = styled.div`
+  ${tw` text-xs`};
+  color: #a2c5a6;
+`
 
 const Footer = () => (
-  <footer className={styles.footer}>
-    <div className="container mx-auto md:flex">
-      <div className="text-center">
-        <div className="text-sm md:text-left pt-6">
-          <h4
-            className="border-b-2 
+  <FooterStyled>
+    <Wrapper>
+      <LeftColumn>
+        <h4
+          className="border-b-2 
           pb-4 mx-4 md:mx-0 md:mb-1
           border-yellow-brand-light md:border-0
           text-2xl md:text-base"
-          >
-            Maplewood Day Camp & Enrichment Center
-          </h4>
-          <a href="tel:508-238-2387">(508) 238-2387</a> -{' '}
-          <a href="mailto:info@maplewoodyearround.com">info@maplewoodyearround.com</a> <br />
-          150 Foundry Street, <br />
-          South Easton, MA, 02375 United States
-        </div>
-      </div>
-      <div className="flex-1 md:pt-4">
-        <p className="flex justify-center md:justify-end px-10 md:px-0">
-          <a href="/" className={styles.icon}>
+        >
+          Maplewood Day Camp & Enrichment Center
+        </h4>
+        <a href="tel:508-238-2387">(508) 238-2387</a> -{' '}
+        <a href="mailto:info@maplewoodyearround.com">info@maplewoodyearround.com</a> <br />
+        150 Foundry Street, <br />
+        South Easton, MA, 02375 United States
+      </LeftColumn>
+      <RightColumn>
+        <Icons>
+          <SocialIcon href="https://www.facebook.com/MaplewoodKids/" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faFacebookSquare} />
-          </a>
-          <a href="/" className={styles.icon}>
+          </SocialIcon>
+          <SocialIcon
+            href="/https://www.instagram.com/maplewood_country_day_camp/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faInstagram} />
-          </a>
-          <a href="/" className={styles.icon}>
+          </SocialIcon>
+          <SocialIcon href="https://www.youtube.com/user/MaplewoodKids" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faYoutube} />
-          </a>
-          <a href="/" className={styles.icon}>
+          </SocialIcon>
+          <SocialIcon href="https://twitter.com/maplewoodkids" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faTwitterSquare} />
-          </a>
-        </p>
-      </div>
-    </div>
-  </footer>
+          </SocialIcon>
+        </Icons>
+      </RightColumn>
+    </Wrapper>
+    <SeoText>
+      Serving Greater Boston area and Rhode Island and located near the following towns: Easton, Sharon, Mansfield,
+      Norton, Canton, Stoughton, Bridgewater, Raynham, Taunton, Foxboro, Brockton, Walpole, Middleboro.
+    </SeoText>
+    <CopyRight>
+      copyright &copy;2013-{new Date().getFullYear()} Maplewood Country Day Camp & Enrichment Center | website by{' '}
+      <a href="https://ovswebsites.com/" target="_blank" rel="noopener noreferrer">
+        OVS Websites
+      </a>
+    </CopyRight>
+  </FooterStyled>
 )
 
 export default Footer
