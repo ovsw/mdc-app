4684cd04c08ebafedd9a8c3362fe182a4dd34dcc
Ovi Savescu <ovi@ovswebsites.com>
2019-04-11 18:51:47 +0300

    CTA block SC refactoring and removed spacing

--- src/components/Content/CtaBlock.js
+++ src/components/Content/CtaBlock.js
@@ -1,13 +1,11 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const CtaBlock = ({ children }) => (
-  <div
-    className="py-6 px-8 sm:mt-20 sm:mb-4 
-bg-green-brand text-white font-primary"
-    style={{ boxShadow: 'inset -5px 0 10px rgba(51, 50, 15, .23)' }}
-  >
-    {children}
-  </div>
-)
+const CtaBlockStyled = styled.div`
+  ${tw`py-6 px-8  bg-green-brand text-white font-primary`};
+  boxshadow: 'inset -5px 0 10px rgba(51, 50, 15, .23)';
+`
+
+const CtaBlock = ({ children }) => <CtaBlockStyled>{children}</CtaBlockStyled>
 
 export default CtaBlock
