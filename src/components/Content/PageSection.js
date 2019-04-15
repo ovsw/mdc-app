b992dc3bc099bcc4570480a78bb84ec66dd65b38
Ovi Savescu <ovi@ovswebsites.com>
2019-04-15 13:20:39 +0300

    adjusted mobile py

--- src/components/Content/PageSection.js
+++ src/components/Content/PageSection.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const PageSectionStyled = styled.div`
-  ${tw`py-16 pb-20 mb-0`};
+  ${tw`py-10 md:py-16 pb-20 mb-0`};
   background-color: ${props => (props.alternate ? '#fdf3e5' : 'transparent')};
 
   border-top: ${props => (props.bordered ? '2px solid green' : 'none')};
