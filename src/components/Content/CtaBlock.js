14c3cf06d055659d9e5a10d97f2184e195261a84
Ovi Savescu <ovi@ovswebsites.com>
2019-04-16 11:23:57 +0300

    adjusted vertical spacing

--- src/components/Content/CtaBlock.js
+++ src/components/Content/CtaBlock.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const CtaBlockStyled = styled.div`
-  ${tw`py-6 px-8  bg-green-brand text-white font-primary`};
+  ${tw`py-16 px-8  bg-green-brand text-white font-primary`};
   boxshadow: 'inset -5px 0 10px rgba(51, 50, 15, .23)';
 `
 
