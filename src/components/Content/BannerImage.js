663975911c4b2ab05857b94296bab8b02fb472da
Ovi Savescu <ovi@ovswebsites.com>
2019-04-16 20:24:23 +0300

    added loader for banner image

--- src/components/Content/BannerImage.js
+++ src/components/Content/BannerImage.js
@@ -3,8 +3,13 @@ import Img from 'gatsby-image'
 import styled from 'styled-components'
 import MainBannerWrapper from 'src/components/Content/MainBannerWrapper'
 
+import LoaderImage from 'src/images/loader.gif'
+
 const BannerImageStyled = styled(Img)`
   ${tw`xl:h-full`};
+  background-image: url('${LoaderImage}');
+  background-position: 50% 40%;
+  background-repeat: no-repeat;
 `
 
 const BannerImage = ({ image, quickLinks }) => (
