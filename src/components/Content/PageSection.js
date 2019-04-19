875f72b7dd8eedfd7dc7f6bce3780b75cfa3723a
Ovi Savescu <ovi@ovswebsites.com>
2019-04-19 21:19:12 +0300

    increased bottom padding

--- src/components/Content/PageSection.js
+++ src/components/Content/PageSection.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
+import media from 'src/components/responsive'
 
 import LightTexture from 'src/images/paint-border-texture-light.png'
 import LightTextureBotom from 'src/images/paint-border-texture-light-bottom.png'
 import LightTextureTop from 'src/images/paint-border-texture-light-top.png'
 
 const PageSectionStyled = styled.div`
-  ${tw`relative py-10 md:py-16  mb-0`};
+  ${tw`relative py-10 md:py-16`};
   background-color: ${props => (props.alternate ? props.theme.lightColorAlt : props.theme.lightColor)};
   border-top: ${props => (props.bordered ? '1px solid #006600' : 'none')};
   ${props => props.topTexture ? 'padding-top: 3rem!important;': ''};
-
+  padding-bottom: 1rem!important;
+  ${media.md`padding-bottom: 3rem!important;`};
   /* background-image: ${props => props.divider ? 'url('+LightTexture+')' : 'none' }; */
   
   &::after {
