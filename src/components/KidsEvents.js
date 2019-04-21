de94932878845d63933c3e50910f30f4c8a6d3b4
Ovi Savescu <ovi@ovswebsites.com>
2019-04-21 20:47:46 +0300

    linted

--- src/components/KidsEvents.js
+++ src/components/KidsEvents.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import styled from 'styled-components'
 import NarrowContent from 'src/components/common/NarrowContent'
 import KidsEvent from 'src/components/KidsEvent'
 
-
 const KidsEvents = ({ events }) => (
-
   <NarrowContent>
     {events.map(event => (
       <KidsEvent key={event.title} event={event} />
