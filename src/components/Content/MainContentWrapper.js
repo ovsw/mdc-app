f7d63279fb72d73482341ce51b5749267f3d9a57
Ovi Savescu <ovi@ovswebsites.com>
2019-04-21 19:50:27 +0300

    linted

--- src/components/Content/MainContentWrapper.js
+++ src/components/Content/MainContentWrapper.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
-import media from 'src/components/responsive'
 
 const MainContentWrapperStyled = styled.div`
   ${tw`xl:w-1/2 xl:mr-auto`};
   border-top: 4px solid ${props => props.theme.primaryColor};
-  
   color: ${props => props.theme.bodyColor};
 `
 
