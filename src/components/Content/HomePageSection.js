07bd950d2e891f2c184e3a081ff78523c68ce20d
Ovi Savescu <ovi@ovswebsites.com>
2019-04-21 19:51:17 +0300

    linted

--- src/components/Content/HomePageSection.js
+++ src/components/Content/HomePageSection.js
@@ -1,13 +1,14 @@
-import React from 'react'
 import styled from 'styled-components'
 import PageSecton from 'src/components/Content/PageSection'
 
 const HomePageSection = styled(PageSecton)`
-  & h1:first-child, & h2:first-child {
+  & h1:first-child,
+  & h2:first-child {
     margin-top: 0;
   }
 
-  & div:last-child, & p:last-child {
+  & div:last-child,
+  & p:last-child {
     margin-bottom: 0;
   }
 `
