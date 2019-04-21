2893fa80e4b18efb3a08338271cd2b995da121ea
Ovi Savescu <ovi@ovswebsites.com>
2019-04-21 20:40:33 +0300

    linted

--- src/components/Templates/FullScreenTemplate.js
+++ src/components/Templates/FullScreenTemplate.js
@@ -4,13 +4,6 @@ import InteractiveMap from 'src/components/Map/InteractiveMap'
 const FullScreenTemplate = ({ locations }) => (
   <>
     <InteractiveMap locations={locations} />
-    {/* <MapFrame
-  title="interactive map"
-  width="100%"
-  height="900"
-  src="https://maphub.net/embed/50364?panel=1"
-  frameBorder="0"
-/> */}
   </>
 )
 
