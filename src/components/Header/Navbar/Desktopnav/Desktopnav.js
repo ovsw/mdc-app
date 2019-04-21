39cafa56133d5dbae4c856edd75eaff01fd3afb2
Ovi Savescu <ovi@ovswebsites.com>
2019-04-21 20:12:47 +0300

    refactored as sfc and linted

--- src/components/Header/Navbar/Desktopnav/Desktopnav.js
+++ src/components/Header/Navbar/Desktopnav/Desktopnav.js
@@ -2,30 +2,17 @@ import React from 'react'
 import Megamenu from 'src/components/Header/Navbar/Desktopnav/Megamenu/Megamenu'
 import CtaLinks from 'src/components/Header/Navbar/CtaLinks/CtaLinks'
 
-class DesktopNav extends React.Component {
-  constructor(props) {
-    super(props)
+const DesktopNav = ({ aboutNav, familiesNav, staffNav, closeAllOverlays }) => (
+  <ul className="list-reset m-0 py-2 px-2 flex items-center">
+    <Megamenu menuTitle="About Us" menuModel={aboutNav} closeAllOverlays={closeAllOverlays} />
+    <Megamenu menuTitle="Maplewood Families" menuModel={familiesNav} closeAllOverlays={closeAllOverlays} />
+    <li className="m-0 text-center flex-auto" />
+    <Megamenu menuTitle="Staff" menuModel={staffNav} closeAllOverlays={closeAllOverlays} />
 
-    this.state = {
-      mobileNavVisible: false,
-    }
-  }
-
-  render() {
-    const { aboutNav, familiesNav, staffNav, closeAllOverlays } = this.props
-    return (
-      <ul className="list-reset m-0 py-2 px-2 flex items-center">
-        <Megamenu menuTitle="About Us" menuModel={aboutNav} closeAllOverlays={closeAllOverlays} />
-        <Megamenu menuTitle="Maplewood Families" menuModel={familiesNav} closeAllOverlays={closeAllOverlays} />
-        <li className="m-0 text-center flex-auto" />
-        <Megamenu menuTitle="Staff" menuModel={staffNav} closeAllOverlays={closeAllOverlays} />
-
-        <li className="m-0 text-center w-2/6">
-          <CtaLinks />
-        </li>
-      </ul>
-    )
-  }
-}
+    <li className="m-0 text-center w-2/6">
+      <CtaLinks />
+    </li>
+  </ul>
+)
 
 export default DesktopNav
