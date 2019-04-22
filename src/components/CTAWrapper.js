da61a08c95f68c2402b429e9afa8f25421fef220
Ovi Savescu <ovi@ovswebsites.com>
2019-04-22 22:56:03 +0300

    fixed z-index that made CTA buttons go on top of the overlays

--- src/components/CTAWrapper.js
+++ src/components/CTAWrapper.js
@@ -5,7 +5,7 @@ import media from 'src/components/responsive'
 const CTAWrapperStyled = styled.div`
   ${tw`flex relative fixed pin-b pin-l pin-r h-12 w-full`}
   ${media.xl`${tw`w-1/2 pin-none pin-r pin-b h-32`}; padding-left: 3px;`};
-  z-index: 9;
+  z-index: 4;
 `
 
 const CTAWrapper = ({ children }) => <CTAWrapperStyled>{children}</CTAWrapperStyled>
