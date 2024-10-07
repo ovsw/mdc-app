c8caed3a893344123ce39a2b6e43cfefebb659a9
OVIS <ovi@ovswebsites.com>
2024-10-07 20:58:52 +0200

    attempt to close all overlays when clicking logo

--- src/components/Logoimage/Logoimage.js
+++ src/components/Logoimage/Logoimage.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { NavContext } from 'src/layouts'
+
 import logo from 'src/images/maplewood-web-logo.png'
 
 import styles from './Logoimage.module.css'
 
 const LogoImage = () => (
-  <Link to="/" className={styles.logo}>
-    <img src={logo} className={styles.image} alt="Maplewood Contry Day Camp logo" />
-  </Link>
+  <NavContext.Consumer>
+    {({ closeAllOverlays }) => (
+      <>
+        <Link to="/" className={styles.logo} onClick={closeAllOverlays}>
+          <img src={logo} className={styles.image} alt="Maplewood Contry Day Camp logo" />
+        </Link>
+      </>
+    )}
+  </NavContext.Consumer>
 )
 
 export default LogoImage
